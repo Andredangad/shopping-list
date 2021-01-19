f8a2c371adf4a373065ea2aa8a0535f049a5cbc6
Andre <andre-marie.dang@fivesgroup.com>
2021-01-19 11:28:54 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+
+class ShoppingList extends React.Component{
+    render(){
+        return(
+        <h1>Bonjour Monde !</h1>
+        )};
+}
+
+class Search extends React.Component{
+    render(){
+        return(
+        <h1>Bonjour Monde !</h1>
+        )};
+}
+
+class Result extends React.Component{
+    render(){
+        return(
+        <h1>Bonjour Monde !</h1>
+        )};
+}
+
+
+class TitleList extends React.Component{
+    render(){
+        return(
+        <h1>Bonjour Monde !</h1>
+        )};
+}
+
+class ResultList extends React.Component{
+    render(){
+        return(
+        <h1>Bonjour Monde !</h1>
+        )};
+}
+
+ReactDOM.render(
+    <ShoppingList />,
+    document.getElementById('root')
+  );
